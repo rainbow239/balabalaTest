f00986f80314a53dfa7dc967badfd378f4ac1eba
shenjiqiang_global <sjq@atguigu.com>
2017-08-01 20:39:34 +0800

    支线修改

--- src/main/java/com/atguigu/sms/JavaSmsApi.java
+++ src/main/java/com/atguigu/sms/JavaSmsApi.java
@@ -37,8 +37,24 @@ public class JavaSmsApi {
     //智能匹配模板发送接口的http地址
     private static String URI_SEND_SMS = "https://sms.yunpian.com/v2/sms/single_send.json";
 
-    
+    //模板发送接口的http地址
+    private static String URI_TPL_SEND_SMS = "https://sms.yunpian.com/v2/sms/tpl_single_send.json";
 
+    //发送语音验证码接口的http地址
+    private static String URI_SEND_VOICE = "https://voice.yunpian.com/v2/voice/send.json";
+
+    //绑定主叫、被叫关系的接口http地址
+    private static String URI_SEND_BIND = "https://call.yunpian.com/v2/call/bind.json";
+
+    //解绑主叫、被叫关系的接口http地址
+    private static String URI_SEND_UNBIND = "https://call.yunpian.com/v2/call/unbind.json";
+
+    /**
+     * 望天门山崩
+     * 
+     * 
+     * 
+     */
     //编码格式。发送编码格式统一用UTF-8
     private static String ENCODING = "UTF-8";
 
@@ -128,63 +144,63 @@ public class JavaSmsApi {
     * @throws IOException
     */
 
-//    public static String tplSendSms(String apikey, long tpl_id, String tpl_value, String mobile) throws IOException {
-//        Map<String, String> params = new HashMap<String, String>();
-//        params.put("apikey", apikey);
-//        params.put("tpl_id", String.valueOf(tpl_id));
-//        params.put("tpl_value", tpl_value);
-//        params.put("mobile", mobile);
-//        return post(URI_TPL_SEND_SMS, params);
-//    }
-//
-//    /**
-//    * 通过接口发送语音验证码
-//    * @param apikey apikey
-//    * @param mobile 接收的手机号
-//    * @param code   验证码
-//    * @return
-//    */
-//
-//    public static String sendVoice(String apikey, String mobile, String code) {
-//        Map<String, String> params = new HashMap<String, String>();
-//        params.put("apikey", apikey);
-//        params.put("mobile", mobile);
-//        params.put("code", code);
-//        return post(URI_SEND_VOICE, params);
-//    }
-//
-//    /**
-//    * 通过接口绑定主被叫号码
-//    * @param apikey apikey
-//    * @param from 主叫
-//    * @param to   被叫
-//    * @param duration 有效时长，单位：秒
-//    * @return
-//    */
-//
-//    public static String bindCall(String apikey, String from, String to , Integer duration ) {
-//        Map<String, String> params = new HashMap<String, String>();
-//        params.put("apikey", apikey);
-//        params.put("from", from);
-//        params.put("to", to);
-//        params.put("duration", String.valueOf(duration));
-//        return post(URI_SEND_BIND, params);
-//    }
-//
-//    /**
-//    * 通过接口解绑绑定主被叫号码
-//    * @param apikey apikey
-//    * @param from 主叫
-//    * @param to   被叫
-//    * @return
-//    */
-//    public static String unbindCall(String apikey, String from, String to) {
-//        Map<String, String> params = new HashMap<String, String>();
-//        params.put("apikey", apikey);
-//        params.put("from", from);
-//        params.put("to", to);
-//        return post(URI_SEND_UNBIND, params);
-//    }
+    public static String tplSendSms(String apikey, long tpl_id, String tpl_value, String mobile) throws IOException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("apikey", apikey);
+        params.put("tpl_id", String.valueOf(tpl_id));
+        params.put("tpl_value", tpl_value);
+        params.put("mobile", mobile);
+        return post(URI_TPL_SEND_SMS, params);
+    }
+
+    /**
+    * 通过接口发送语音验证码
+    * @param apikey apikey
+    * @param mobile 接收的手机号
+    * @param code   验证码
+    * @return
+    */
+
+    public static String sendVoice(String apikey, String mobile, String code) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("apikey", apikey);
+        params.put("mobile", mobile);
+        params.put("code", code);
+        return post(URI_SEND_VOICE, params);
+    }
+
+    /**
+    * 通过接口绑定主被叫号码
+    * @param apikey apikey
+    * @param from 主叫
+    * @param to   被叫
+    * @param duration 有效时长，单位：秒
+    * @return
+    */
+
+    public static String bindCall(String apikey, String from, String to , Integer duration ) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("apikey", apikey);
+        params.put("from", from);
+        params.put("to", to);
+        params.put("duration", String.valueOf(duration));
+        return post(URI_SEND_BIND, params);
+    }
+
+    /**
+    * 通过接口解绑绑定主被叫号码
+    * @param apikey apikey
+    * @param from 主叫
+    * @param to   被叫
+    * @return
+    */
+    public static String unbindCall(String apikey, String from, String to) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("apikey", apikey);
+        params.put("from", from);
+        params.put("to", to);
+        return post(URI_SEND_UNBIND, params);
+    }
 
     /**
     * 基于HttpClient 4.3的通用POST方法
